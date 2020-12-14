10d267f84759309240adf35103313c48a905ba9d
bittergourdking <jkang21@stuy.edu>
2020-12-14 01:09:08 -0500

    changed add code and tested

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,29 +8,39 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(initialCapacity);
   }
   //methods
-  private int findIndex(T element) {
+  private int findIndex(T element, int size) {
     if (element == null) {
-      throw new IllegalArgumentException("No nulls allowed.");
+      throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
     int index = 0;
-    while (index < size() - 1
-        && element.compareTo(get(index + 1)) < 0) {
-      index++;
+    for (int i = 0; i < size; i++) {
+      if (super.get(i).compareTo(element) < 0) {
+        i++;
+      }
     }
     return index;
   }
 
   public boolean add(T element) {
-    super.add(findIndex(element), element);
-    return true;
+    if (element == null) {
+      throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
+    }
+    int size = this.size();
+    for (int i = 0; i < size; i++) {
+      if (element.compareTo(this.get(i)) <= 0) {
+        super.add(i, element);
+        return true;
+      }
+    }
+    return super.add(element);
   }
 
   public void add(int index, T element) {
-    super.add(findIndex(element), element);
+    super.add(findIndex(element, this.size()), element);
   }
 
   public T set(int index, T element) {
-    return super.set(findIndex(element), element);
+    return super.set(findIndex(element, this.size()), element);
   }
 
 }
